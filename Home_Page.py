1157ea741ea37e7a107b2b3d840a9ce658f97141
RavitejaRuthala <ruthalaraviteja@gmail.com>
2024-06-05 09:51:47 -0400

    Removed streamlit default Menu and other unnecessary elements and added additional UI elements for better user experience.

--- Home_Page.py
+++ Home_Page.py
@@ -1,21 +1,20 @@
 import streamlit as st
-import datetime
+from support_file import *
 
-def reset_value():
-    st.session_state.author_name = None
-    st.session_state.author_email = None
-    st.session_state.meeting_agenda = None
-    st.session_state.meeting_notes = None
-    
 st.set_page_config(page_title="Minutes of Meeting", page_icon="📋")
-st.markdown("# Minutes of Meeting - Form")
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.header("Minutes of Meeting - Form")
 with st.form("MofM_form"):
-   st.text_input("**Author's Name:**", placeholder="Enter your name here.", key="author_name")
-   st.text_input("**Author's Email ID:**", placeholder="Enter your email address here.", key="author_email")
-   st.date_input("**Date of the Meeting:**", value="today", format="MM/DD/YYYY", key="meeting_date")
-   st.text_input("**Agenda of the Meeting:**", placeholder="Specify the meeting's agenda here.", key="meeting_agenda")
-   st.text_area("**Minutes of the Meeting:**", placeholder="Mention the meeting minutes here.", key="meeting_notes")
-   submitted = st.form_submit_button("Submit")
-   st.form_submit_button('Reset', on_click=reset_value)
+   input_name = st.text_input("**Author's Name:**", placeholder="Enter your name here.", key="author_name", max_chars=50)
+   input_email = st.text_input("**Author's Email ID:**", placeholder="Enter your email address here.", key="author_email", max_chars=50)
+   input_date = st.date_input("**Date of the Meeting:**", value="today", format="MM/DD/YYYY", key="meeting_date")
+   input_agenda = st.text_input("**Agenda of the Meeting:**", placeholder="Specify the meeting's agenda here.", key="meeting_agenda")
+   input_notes = st.text_area("**Minutes of the Meeting:**", placeholder="Mention the meeting minutes here.", key="meeting_notes")
+   columns = st.columns([12,1.5], gap="small")
+   with columns[0]:
+    submitted = st.form_submit_button("Submit")
+   with columns[1]:
+    st.form_submit_button('Reset', on_click=reset_value)
    if submitted:
-       st.success('Submitted successfully!', icon="✅")
+       st.toast('Submitted successfully!', icon="✅")
+       st.balloons()
