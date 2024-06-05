37fd370c14dcfed6135a0fd2fa34cb760197fabd
RavitejaRuthala <ruthalaraviteja@gmail.com>
2024-06-05 09:52:59 -0400

    Adding user defined function file.

---
+++ support_file.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+""" 
+
+def reset_value():
+    st.session_state.author_name = None
+    st.session_state.author_email = None
+    st.session_state.meeting_agenda = None
+    st.session_state.meeting_notes = None
+    
+def input_validation():
+    st.write("Need to code!!")
