fc1addb8be2c377b18773661ccc5320cc0cc4537
RavitejaRuthala <ruthalaraviteja@gmail.com>
2024-06-19 16:56:41 -0400

    Resolved an issue with Update functionality.

--- pages/3_Update_Meeting_Notes.py
+++ pages/3_Update_Meeting_Notes.py
@@ -10,7 +10,8 @@ if searched:
     else:
         fetched_result = fetch_meeting_notes(unique_code)
         if fetched_result != None:
-            edit_meeting_notes(unique_code, fetched_result)       
+            edit_meeting_notes(unique_code, fetched_result)  
+            st.session_state.unique_code_field = None     
         else:
             st.warning("Oops!! there is no such meeting ID with us.", icon="⚠️")
-        st.session_state.unique_code_field = None
+            st.session_state.unique_code_field = None
