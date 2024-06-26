c5860174126860d9c1e1057b816e828470ba19f2
RavitejaRuthala <ruthalaraviteja@gmail.com>
2024-06-26 16:10:49 -0400

    Added retrieve functionality.

--- pages/4_Retrieve_Meeting_IDs.py
+++ pages/4_Retrieve_Meeting_IDs.py
@@ -6,6 +6,6 @@ with st.form("Retrieve_IDs", clear_on_submit=True):
     email_address = st.text_input("**Enter your Email ID**", placeholder="Type your email address here...", key="email_ID", max_chars=50)
     columns = st.columns([12, 1.5], gap="small")
     with columns[0]:
-        retrieved = st.form_submit_button("Send meeting IDs to my email ID")
+        retrieved = st.form_submit_button("Retrieve associated Meeting IDs")
     with columns[1]:
         reseted = st.form_submit_button('Reset')
